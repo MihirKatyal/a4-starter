583eec0fdaf6ae96f68bb8898f0d11a2222fa7cc
Mihir Katyal <mkatyal@uci.edu>
2024-03-08 13:56:22 -0800

    imported libraries and created class OpenWeather

--- OpenWeather.py
+++ OpenWeather.py
@@ -1,11 +1,18 @@
-# openweather.py
+import urllib.request
+import urllib.error
+import json
 
-# Starter code for assignment 4 in ICS 32
-# Programming with Software Libraries in Python
-
-# Replace the following placeholders with your information.
-
-# NAME
-# EMAIL
-# STUDENT ID
-
+class OpenWeather:
+    def __init__(self, zip_code: str, country_code: str):
+        self.zip_code = zip_code
+        self.country_code = country_code
+        self.apikey = None
+        self.temperature = None
+        self.high_temperature = None
+        self.low_temperature = None
+        self.longitude = None
+        self.latitude = None
+        self.description = None
+        self.humidity = None
+        self.sunset = None
+        self.city = None
