491a98a0c4e283729e321f9884374e22db5f0089
Mihir Katyal <mkatyal@uci.edu>
2024-03-08 18:04:58 -0800

    refactroed code for opemweather.py

--- OpenWeather.py
+++ OpenWeather.py
@@ -1,61 +1,41 @@
 import urllib.request
 import urllib.error
 import json
+from WebAPI import WebAPI
 
-class OpenWeather:
-    def __init__(self, zip_code: str, country_code: str):
+class OpenWeather(WebAPI):
+    def __init__(self, zip_code='92617', country_code='US'):
+        super().__init__()
         self.zip_code = zip_code
         self.country_code = country_code
-        self.apikey = None
-        self.temperature = None
-        self.high_temperature = None
-        self.low_temperature = None
-        self.longitude = None
-        self.latitude = None
-        self.description = None
-        self.humidity = None
-        self.sunset = None
-        self.city = None
+        # Initializing attributes to store weather data
+        self.temperature = 0
+        self.high_temperature = 0
+        self.low_temperature = 0
+        self.longitude = 0
+        self.latitude = 0
+        self.description = ''
+        self.humidity = 0
+        self.sunset = 0
+        self.city = ''
 
-    def set_apikey(self, apikey: str) -> None:
-        self.apikey = apikey
-
-    def load_data(self) -> None:
-        if not self.apikey:
-            raise ValueError("API key is not set.")
-        
-        url = f"http://api.openweathermap.org/data/2.5/weather?zip={self.zip_code},{self.country_code}&appid={self.apikey}&units=metric"
-        try:
-            response = urllib.request.urlopen(url)
-            data = json.loads(response.read())
-            # Assign data to attributes
-            self.temperature = data['main']['temp']
-            self.high_temperature = data['main']['temp_max']
-            self.low_temperature = data['main']['temp_min']
-            self.longitude = data['coord']['lon']
-            self.latitude = data['coord']['lat']
-            self.description = data['weather'][0]['description']
-            self.humidity = data['main']['humidity']
-            sunset_time = data['sys']['sunset']
-            self.city = data['name']
-            self.sunset = sunset_time  # You might want to format this timestamp
-        except urllib.error.URLError as e:
-            raise ConnectionError("There was an error connecting to the internet or the API URL was incorrect.")
-        except urllib.error.HTTPError as e:
-            if e.code == 404:
-                raise ConnectionError("Requested data not found (404 error).")
-            elif e.code == 503:
-                raise ConnectionError("Service unavailable (503 error).")
-            else:
-                raise ConnectionError(f"HTTP error encountered: {e.code}")
-        except json.JSONDecodeError:
-            raise ValueError("Error processing JSON data from API.")
-        except KeyError:
-            raise ValueError("Incomplete or incorrect data received from API.")
+    def load_data(self):
+        url = f"http://api.openweathermap.org/data/2.5/weather?zip={self.zip_code},{self.country_code}&appid={self.api_key}&units=metric"
+        data = self._download_url(url)
+        # Assigning data to attributes
+        self.temperature = data['main']['temp']
+        self.high_temperature = data['main']['temp_max']
+        self.low_temperature = data['main']['temp_min']
+        self.longitude = data['coord']['lon']
+        self.latitude = data['coord']['lat']
+        self.description = data['weather'][0]['description']
+        self.humidity = data['main']['humidity']
+        self.sunset = data['sys']['sunset']
+        self.city = data['name']
 
     def transclude(self, message: str) -> str:
         if '@weather' in message:
-            self.load_data()  # Ensure data is loaded
-            weather_data = f"{self.description}, {self.temperature}°C (high of {self.high_temperature}°C, low of {self.low_temperature}°C)"
+            self.load_data()  # Make sure data is loaded
+            weather_data = f"the weather in {self.city} is {self.description} with a temperature of {self.temperature}°C, high of {self.high_temperature}°C, and low of {self.low_temperature}°C."
             message = message.replace('@weather', weather_data)
         return message
