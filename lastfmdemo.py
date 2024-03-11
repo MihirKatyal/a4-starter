e3f0eea27ba1b498c48b94f1f6a9d97d071d7de5
Mihir Katyal <mkatyal@uci.edu>
2024-03-10 23:11:13 -0700

    upadted last fm demo to test

--- lastfmdemo.py
+++ lastfmdemo.py
@@ -1,14 +1,14 @@
 from LastFM import LastFM
 
 def main():
-    user = "MihirKatyal" 
-    api_key = "43effd57c3bbcfc8d88897c50b1cb0cf"  
+    user = "MihirKatyal"
+    api_key = "43effd57c3bbcfc8d88897c50b1cb0cf"
 
     last_fm = LastFM(user)
     last_fm.set_apikey(api_key)
     try:
-        last_fm.load_top_tracks()
-        last_fm.print_top_tracks()
+        last_fm.load_data()  # Updated from last_fm.load_top_tracks()
+        last_fm.print_top_tracks()  
     except ConnectionError as e:
         print(f"Error: {str(e)}")
     except ValueError as e:
